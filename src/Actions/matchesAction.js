ee0b4de8dbffd24a44866bd286e9d55c031eb2a7
Sylene Manusset <sylene.manusset@gmail.com>
2019-06-27 17:22:36 +0200

    corrected name of the fetchMatches const in matchesAction

--- src/Actions/matchesAction.js
+++ src/Actions/matchesAction.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { fetchIsLoading, fetchSuccess, fetchError } from './fetchActions';
 
-export const fetchWilders = () => {
+export const fetchMatches = () => {
   return async (dispatch) => {
     dispatch(fetchIsLoading());
     axios
