3afd55f81c77a8e65b06ddbf4520485624736866
EnzoDuhalde <enzo-duhalde@hotmail.fr>
2019-06-28 03:53:11 +0200

    [WIP] display the matches with key, still need to display the complete names of the campuses

--- src/Components/MatchDualComponent.jsx
+++ src/Components/MatchDualComponent.jsx
@@ -4,45 +4,22 @@ import { connect } from 'react-redux';
 
 import { fetchCampuses } from '../Actions/campusesAction';
 
-class MatchDual extends React.Component {
-  componentDidMount() {
-    this.props.fetchCampuses();
-  }
-  render() {
-    const { error, isLoading, data } = this.props;
-    if (error) {
-      return <div>Error! {error.message}</div>;
-    } else if (isLoading) {
-      return <div>Loading ...</div>;
-    } else {
-      return (
-        <Row>
-          <Card style={{ width: '18rem' }}>
-            <Card.Header>Domicile</Card.Header>
-            <ListGroup variant="flush">
-              {data.map((result, index) => (
-                <div>
-                  {' '}
-                  {result.uid === this.props.homeTeam ? (
-                    <ListGroup.Item key={index}>{result.name}</ListGroup.Item>
-                  ) : null}
-                </div>
-              ))}
-            </ListGroup>
-          </Card>
-          <Card style={{ width: '18rem' }}>
-            <Card.Header>Visiteur</Card.Header>
-            <ListGroup variant="flush">
-              <ListGroup.Item>
-                {data.map((item, index) => item.name)}
-              </ListGroup.Item>
-            </ListGroup>
-          </Card>
-        </Row>
-      );
-    }
-  }
-}
+const MatchDual = (props) => {
+  return (
+    <Row>
+      <Card style={{ width: '18rem' }}>
+        <Card.Header>Domicile</Card.Header>
+        <ListGroup variant="flush">{props.homeTeam}</ListGroup>
+      </Card>
+      <Card style={{ width: '18rem' }}>
+        <Card.Header>Visiteur</Card.Header>
+        <ListGroup variant="flush">
+          <ListGroup.Item>{props.awayTeam}</ListGroup.Item>
+        </ListGroup>
+      </Card>
+    </Row>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
